67a30e5922896f1e99438d712cbe34dba3fa1a97
Asish Kumar <87874775+officialasishkumar@users.noreply.github.com>
2023-05-11 23:17:31 +0530

    Create smallest_prime_factors.cpp

---
+++ templates/smallest_prime_factors.cpp
@@ -0,0 +1,7 @@
+vector<int> spf(1e6+1, 0);
+for (int i = 0; i <= 1e6; i++)
+        spf[i] = i;
+    for (int i = 2; i <= 1e6; i++)
+        if (spf[i] == i)
+            for (int j = i; j <= 1e6; j += i)
+                spf[j] = i;
